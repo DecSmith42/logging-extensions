f45c85c475d1101e363def4b8029fb4cc0ce5612
Declan Smith <dec.smith42@gmail.com>
2025-04-24 15:54:12 +1000

    fix(tests): update TestTimeProvider to use custom time zone in tests
    The LocalTimeZone property now returns a custom time zone ("TestTimeZone")
    instead of the system's local time zone. This ensures consistency in test
    results regardless of the environment's time zone configuration.

--- DecSm.Extensions.Logging.File.UnitTests/TestTimeProvider.cs
+++ DecSm.Extensions.Logging.File.UnitTests/TestTimeProvider.cs
@@ -4,7 +4,8 @@ public sealed class TestTimeProvider : TimeProvider
 {
     public DateTimeOffset UtcNow { get; set; } = new(2020, 1, 1, 0, 0, 0, TimeSpan.Zero);
 
-    public override TimeZoneInfo LocalTimeZone => TimeZoneInfo.Local;
+    public override TimeZoneInfo LocalTimeZone =>
+        TimeZoneInfo.CreateCustomTimeZone("TTZ", TimeSpan.FromHours(10), "TestTimeZone", "TestTimeZone");
 
     public override DateTimeOffset GetUtcNow() =>
         UtcNow;
